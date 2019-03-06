059582ad170d1d391e497a7cd467306a3a8964b6
John Adjei <john@johnadjei.com>
2019-03-06 14:56:04 -0500

    Add dev dependencies and test packages

--- setup.py
+++ setup.py
@@ -1,109 +1,74 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import io
+# Learn more: https://github.com/kennethreitz/setup.py
 import os
 import sys
-from shutil import rmtree
 
-from setuptools import find_packages, setup, Command
+from codecs import open
 
-# Package meta-data.
-NAME = 'memcache-filestore'
-DESCRIPTION = 'A Python library to store & retrieve files using Memcache'
-URL = 'https://github.com/johnadjei/memcache-filestore'
-EMAIL = 'john@johnadjei.com'
-AUTHOR = 'John Adjei'
-VERSION = None
-
-# What packages are required for this module to be executed?
-REQUIRED = [
-    'pymemcache'
-]
-
-# What packages are optional?
-EXTRAS = {
-
-}
-
-# The rest you shouldn't have to touch too much :)
-# ------------------------------------------------
-# Except, perhaps the License and Trove Classifiers!
-# If you do change the License, remember to change the Trove Classifier for that!
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
 
 here = os.path.abspath(os.path.dirname(__file__))
 
-# Import the README and use it as the long-description.
-# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
-try:
-    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
-        long_description = '\n' + f.read()
-except FileNotFoundError:
-    long_description = DESCRIPTION
 
-# Load the package's __version__.py module as a dictionary.
-about = {}
-if not VERSION:
-    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
-    with open(os.path.join(here, project_slug, '__version__.py')) as f:
-        exec(f.read(), about)
-else:
-    about['__version__'] = VERSION
-
-
-class UploadCommand(Command):
-    """Support setup.py upload."""
-
-    description = 'Build and publish the package.'
-    user_options = []
-
-    @staticmethod
-    def status(s):
-        """Prints things in bold."""
-        print('\033[1m{0}\033[0m'.format(s))
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass into py.test")]
 
     def initialize_options(self):
-        pass
+        TestCommand.initialize_options(self)
+        try:
+            from multiprocessing import cpu_count
+            self.pytest_args = ['-n', str(cpu_count()), '--boxed']
+        except (ImportError, NotImplementedError):
+            self.pytest_args = ['-n', '1', '--boxed']
 
     def finalize_options(self):
-        pass
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
 
-    def run(self):
-        try:
-            self.status('Removing previous builds…')
-            rmtree(os.path.join(here, 'dist'))
-        except OSError:
-            pass
+    def run_tests(self):
+        import pytest
 
-        self.status('Building Source and Wheel (universal) distribution…')
-        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
-
-        self.status('Uploading the package to PyPI via Twine…')
-        os.system('twine upload dist/*')
-
-        self.status('Pushing git tags…')
-        os.system('git tag v{0}'.format(about['__version__']))
-        os.system('git push --tags')
-
-        sys.exit()
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 
-# Where the magic happens:
+# 'setup.py publish' shortcut.
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist bdist_wheel')
+    os.system('twine upload dist/*')
+    sys.exit()
+
+packages = ['memcache_filestore']
+requires = ['pymemcache>=2.1.1']
+test_requirements = ['pytest-cov', 'pytest-mock', 'pytest>=2.8.0']
+
+about = {}
+with open(os.path.join(here, 'memcache_filestore', '__version__.py'),
+          'r', 'utf-8') as f:
+    exec(f.read(), about)
+
+with open('README.md', 'r', 'utf-8') as f:
+    readme = f.read()
+
 setup(
     name=about['__title__'],
     version=about['__version__'],
     description=about['__description__'],
-    long_description=long_description,
+    long_description=readme,
     long_description_content_type='text/markdown',
     author=about['__author__'],
     author_email=about['__author_email__'],
-    python_requires=">=3.4, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
     url=about['__url__'],
-    packages=find_packages(exclude=('tests',)),
-    install_requires=REQUIRED,
-    extras_require=EXTRAS,
+    packages=packages,
+    package_data={'': ['LICENSE'], 'memcache_filestore': ['*.pem']},
+    package_dir={'memcache_filestore': 'memcache_filestore'},
     include_package_data=True,
+    python_requires=">=3.4",
+    install_requires=requires,
     license=about['__license__'],
+    zip_safe=False,
     classifiers=[
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
@@ -113,8 +78,7 @@ setup(
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7'
     ],
-    # $ setup.py publish support.
-    cmdclass={
-        'upload': UploadCommand,
-    },
+    cmdclass={'test': PyTest},
+    tests_require=test_requirements,
+    extras_require={},
 )
